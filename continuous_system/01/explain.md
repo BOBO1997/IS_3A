3ddf19ed128875883f3b2516a05caf98b94a70c9
BOBO1997git <yh198595@gmail.com>
2018-10-04 13:14:20 +0900

    changed figure

--- continuous_system/01/explain.md
+++ continuous_system/01/explain.md
@@ -31,6 +31,6 @@
 
 <img src = "https://latex.codecogs.com/gif.latex?O(N&space;&plus;&space;\frac{N}{p}\log&space;{\frac{N}{p}})" />
 
-のようになります。これは、ほぼ線形に増えていく計算量で、実験結果とよく合致しますね。式からもわかるように、特にNがpに対して大きい時に、線形性が顕著になります。
+のようになります。これは、nlognで増えていく計算量で、係数が小さくなります。下のグラフを見ると、実験結果とよく合致しますね(諸説)
 
 - 上の表をグラフにしたものは[figure.png](https://github.com/BOBO1997/IS_3A/blob/master/continuous_system/01/figure.png)にあります。
--- continuous_system/01/plot.py
+++ continuous_system/01/plot.py
@@ -1,15 +1,23 @@
 import matplotlib
 matplotlib.use('Agg')
+from matplotlib import pyplot, pylab
 import matplotlib.pyplot as plt
 import numpy as np
 
 num, time = np.loadtxt("./data.txt", unpack = True)
 
-approximate = np.polyfit(num, time, 1)
+# approximate = np.polyfit(num, time, 1)
 
-linear = np.poly1d(approximate)(num)
+def y(x):
+	x.astype(float)
+	return (np.log2(x / 128) * x / 128 + 2 * x) * 0.001 + 0.035
 
-plt.plot(num, linear, label = 'linear regression')
+# linear = np.poly1d(approximate)(num)
+
+# plt.plot(num, linear, label = 'linear regression')
+
+plt.plot(num, y(num), label = '((n/128)log(n/128) + 2n) * 0.001 +  0.035')
+
 plt.scatter(num, time, label = 'raw data')
 plt.xlabel("input size")
 plt.ylabel("time [sec]")
