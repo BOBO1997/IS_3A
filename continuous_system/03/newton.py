62737d596ed26ef165f338fae37960a4166958d7
BOBO1997git <yh198595@gmail.com>
2018-10-27 01:52:56 +0900

    fixed newton.py

--- continuous_system/03/newton.py
+++ continuous_system/03/newton.py
@@ -2,14 +2,21 @@ import os
 import random
 import math as m
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 def f(x):
-	return m.exp(x ** 4) - m.pi
+	try:
+		e = m.exp(x ** 4)
+	except:
+		e = float('inf')
+	return e - m.pi
 
 def diff(x):
-	return m.exp(x ** 4) * 4 * (x ** 3)
+	try:
+		e = m.exp(x ** 4)
+	except:
+		e = float('inf')
+	return e * 4 * (x ** 3)
 	
 def newton(f, diff, init_value, threshold, max_iter, file_name):
 	iter_list = []
@@ -29,31 +36,22 @@ def newton(f, diff, init_value, threshold, max_iter, file_name):
 	plt.title("Using Newton's Method")
 	plt.savefig(file_name)
 
-def slow_newton(f, diff, init_value, weight, param, threshold, max_iter, file_name):
+def slow_newton(f, diff, init_value, weight, param, threshold, max_iter,file_name):
 	iter_list = []
 	val = init_value
 	val_list = []
 	for i in range(max_iter):
-		limit = 0
-		try:
-			l = abs(f(val - weight ** limit * (f(val) / diff(val))))
-		except OverflowError:
-			l = float('inf')
-			pass
-		r = (1 - param * (weight ** limit)) * abs(f(val))
+		weight_power = 0
+		l = abs(f(val - weight ** weight_power * (f(val) / diff(val))))
+		r = (1 - param * (weight ** weight_power)) * abs(f(val))
 		while l >= r or l == float('inf'):
-			if (limit > -100):
-				limit -= 1
+			if (weight_power > - max_iter):
+				weight_power -= 1
 			else:
 				break
-			try:
-				l = abs(f(val - weight ** limit * f(val) / diff(val)))
-			except OverflowError:
-				l = float('inf')
-				pass
-			r = (1 - param * (weight ** limit)) * abs(f(val))
-		print(val)
-		val = val - (weight ** limit) * (f(val) / diff(val))
+			l = abs(f(val - weight ** weight_power * f(val) / diff(val)))
+			r = (1 - param * (weight ** weight_power)) * abs(f(val))
+		val = val - (weight ** weight_power) * (f(val) / diff(val))
 		err = f(val)
 		if abs(err) > threshold:
 			iter_list.append(i)
@@ -68,14 +66,6 @@ def slow_newton(f, diff, init_value, weight, param, threshold, max_iter, file_na
 
 if __name__ == '__main__':
 	init_value = random.uniform(-2.0, 2.0)
-	init_value = 0.3
-	try:
-		newton(f, diff, init_value, 0.0000000001, 100, 'newton.png')
-	except OverflowError:
-		print("f(x) became too big number")
-		pass
-	#try:
+	init_value = 0.1
 	slow_newton(f, diff, init_value, 20, 0.9, 0.0000000001, 10000, 'slow_newton.png')
-	#except OverflowError:
-		#print("f(x) became too big number")
-		# pass
+	newton(f, diff, init_value, 0.0000000001, 10000, 'newton.png')
