3a793d047c80e4c6732d1161f7a01216e5a22294
BOBO1997git <yh198595@gmail.com>
2018-11-02 16:33:37 +0900

    accelerated orbit speed

--- continuous_system/03/rk_animation.py
+++ continuous_system/03/rk_animation.py
@@ -16,21 +16,17 @@ def rungekutta(m1, m2, r1, r2, v1, v2, iters, h):
 	r = r1 - r2
 	v = v1 - v2
 	orbitr = r
-	orbit1 = r * (m2 / (m1 + m2))# np.array([])
-	orbit2 = r * (- m1 / (m1 + m2)) # np.array([])
+	orbit1 = r * (m2 / (m1 + m2))
+	orbit2 = r * (- m1 / (m1 + m2))
 	for i in range(iters - 1):
 		ar = f(v)
 		av = g(r, m1, m2)
-
 		br = f(v + av * h / 2)
 		bv = g(r + ar * h / 2, m1, m2)
-		
 		cr = f(v + bv * h / 2)
 		cv = g(r + br * h / 2, m1, m2) 
-		
 		dr = f(v + cv * h)
 		dv = g(r + cr * h, m1, m2)
-		
 		r = r + h * (ar + 2 * br + 2 * cr + dr) / 6
 		v = v + h * (av + 2 * bv + 2 * cv + dv) / 6
 		orbit1 = np.append(orbit1, r * ( m2 / (m1 + m2)))
@@ -39,7 +35,7 @@ def rungekutta(m1, m2, r1, r2, v1, v2, iters, h):
 	return np.reshape(orbit1, (iters, 2)), np.reshape(orbit2, (iters, 2)), np.reshape(orbitr, (iters, 2))
 
 if __name__ == '__main__':
-	iters = 10000
+	iters = 1000
 	h = 0.01
 	m1 = 1
 	m2 = 1
@@ -53,14 +49,10 @@ if __name__ == '__main__':
 	x1, y1 = orbit1.T
 	x2, y2 = orbit2.T
 	xr, yr = orbitr.T
-	#plt.plot(x1, y1)
-	#plt.plot(x2, y2)
 	
 	fig = plt.figure()
-	#fig2 = plt.figure()
 	ims = []
-	#ims2 = []
-	for i in range(1000):
+	for i in range(iters):
 		x1_array = np.array([x1[i]])
 		y1_array = np.array([y1[i]])
 		x2_array = np.array([x2[i]])
@@ -70,18 +62,10 @@ if __name__ == '__main__':
 		im3 = plt.plot(x1, y1, "r")
 		im4 = plt.plot(x2, y2, "b")
 		ims.append(im1 + im2 + im3 + im4)
-		#ims2.append([im2])
 		print(i)
-	ani = animation.ArtistAnimation(fig, ims, interval = 10)
-	#ani2 = animation.ArtistAnimation(fig2, ims2, interval = 100)
+	ani = animation.ArtistAnimation(fig, ims, interval = 5)
 	#plt.show()
 
 	plt.xlabel("x")
 	plt.ylabel("y")
-	#ax=plt.colorbar()#カラーマップの凡例
-	#ax.set_label('time [10 ^ (-5) * sec]')#カラーバーのラベルネーム
-	#plt.plot(xr, yr)
-	#plt.plot(x1, y1)
-	#plt.plot(x2, y2)
-	#plt.savefig("orbit.html")
 	ani.save("output.html", writer="imagemagick")
