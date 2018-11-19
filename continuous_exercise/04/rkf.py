80b6ff19cf047cc1de586f467713beeff852a3ea
BOBO1997git <yh198595@gmail.com>
2018-11-20 04:05:29 +0900

    added rkf codes

--- continuous_exercise/04/rkf.py
+++ continuous_exercise/04/rkf.py
@@ -1,73 +1,90 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import matplotlib.animation as animation
 
+class RKF:
+	def __init__(self, alpha, error, h, max_iter, x, y1, y2):
+		self.h = h
+		self.max_iter = max_iter
+		self.x = x
+		self.y1 = y1
+		self.y2 = y2
+		self.alpha = alpha
+		self.error = error
 
-def accurate(x):
-	return - math.exp(- x) / 2 + 2 * math.exp(- 2 * x) / 5 + 3 * math.sin(x) / 10 + math.cos(x) / 10
+	def fy1(self, y2):
+		return y2
 
-def f(v):
-	return v
+	def fy2(self, x, y1, y2):
+		return - 3 * y2 - 2 * y1 + math.cos(x)
 
-def g(r, m1, m2):
-	return (- G * (m1 + m2) / (np.linalg.norm(r) ** 3)) * r
+	def rkf_test(self, h):
+		a1 = self.fy1(self.y2)
+		a2 = self.fy2(self.x, self.y1, self.y2)
 
-def rungekutta(m1, m2, r1, r2, v1, v2, iters, h):
-	r = r1 - r2
-	v = v1 - v2
-	orbitr = r
-	orbit1 = r * (m2 / (m1 + m2))
-	orbit2 = r * (- m1 / (m1 + m2))
-	for i in range(iters - 1):
-		ar = f(v)
-		av = g(r, m1, m2)
-		br = f(v + av * h / 2)
-		bv = g(r + ar * h / 2, m1, m2)
-		cr = f(v + bv * h / 2)
-		cv = g(r + br * h / 2, m1, m2) 
-		dr = f(v + cv * h)
-		dv = g(r + cr * h, m1, m2)
-		r = r + h * (ar + 2 * br + 2 * cr + dr) / 6
-		v = v + h * (av + 2 * bv + 2 * cv + dv) / 6
-		orbit1 = np.append(orbit1, r * ( m2 / (m1 + m2)))
-		orbit2 = np.append(orbit2, - r * ( m1 / (m1 + m2)))
-		orbitr = np.append(orbitr, r)
-	return np.reshape(orbit1, (iters, 2)), np.reshape(orbit2, (iters, 2)), np.reshape(orbitr, (iters, 2))
+		b1 = self.fy1(self.y2 + a2 / 4)
+		b2 = self.fy2(self.x + h / 4, 
+					  self.y1 + a1 / 4, 
+					  self.y2 + a2 / 4)
 
-if __name__ == '__main__':
-	iters = 1000
-	h = 0.01
-	m1 = 1
-	m2 = 1
-	r1 = np.array([1, 0])
-	r2 = np.array([0, 0])
-	v1 = np.array([0, 0.6])
-	v2 = np.array([0, -0.6])
-	orbit1, orbit2, orbitr = rungekutta(m1, m2, r1, r2, v1, v2, iters, h)
-	print(orbit1.shape)
-	t = np.linspace(0, 1, iters)
-	x1, y1 = orbit1.T
-	x2, y2 = orbit2.T
-	xr, yr = orbitr.T
-	
-	fig = plt.figure()
-	ims = []
-	for i in range(iters):
-		x1_array = np.array([x1[i]])
-		y1_array = np.array([y1[i]])
-		x2_array = np.array([x2[i]])
-		y2_array = np.array([y2[i]])
-		im1 = plt.plot(x1_array, y1_array, "r", marker = "o")
-		im2 = plt.plot(x2_array, y2_array, "b", marker = "o")
-		im3 = plt.plot(x1, y1, "r")
-		im4 = plt.plot(x2, y2, "b")
-		ims.append(im1 + im2 + im3 + im4)
-		print(i)
-	ani = animation.ArtistAnimation(fig, ims, interval = 5)
-	#plt.show()
+		c1 = self.fy1(self.y2 + 3 * a2 / 32 + 9 * a2 / 32)
+		c2 = self.fy2(self.x + 3 * h / 8,
+					  self.y1 + 3 * a1 / 32 + 9 * a1 / 32,
+					  self.y2 + 3 * a2 / 32 + 9 * a2 / 32)
 
+		d1 = self.fy1(self.y2 + 1932 * a2 / 2197 - 7200 * b2 / 2197 + 7296 * c2 / 2197)
+		d2 = self.fy2(self.x + 12 * h / 13,
+					  self.y1 + 1932 * a1 / 2197 - 7200 * b1 / 2197 + 7296 * c1 / 2197,
+					  self.y2 + 1932 * a2 / 2197 - 7200 * b2 / 2197 + 7296 * c2 / 2197)
+
+		e1 = self.fy1(self.y2 + 439 * a2 / 216 - 8 * b2 + 3680 * c2 / 513 - 845 * d2 / 4104)
+		e2 = self.fy2(self.x + h,
+					  self.y1 + 439 * a1 / 216 - 8 * b1 + 3680 * c1 / 513 - 845 * d1 / 4104,
+					  self.y2 + 439 * a2 / 216 - 8 * b2 + 3680 * c2 / 513 - 845 * d2 / 4104)
+
+		f1 = self.fy1(self.y2 - 8 * a1 / 27 + 2 * b2 - 3544 * c2 / 2565 + 1859 * d2 / 4104 - 11 * e2 / 40)
+		f2 = self.fy2(self.x + h / 2,
+					  self.y2 - 8 * a1 / 27 + 2 * b1 - 3544 * c1 / 2565 + 1859 * d1 / 4104 - 11 * e1 / 40,
+					  self.y2 - 8 * a2 / 27 + 2 * b2 - 3544 * c2 / 2565 + 1859 * d2 / 4104 - 11 * e2 / 40)
+		rkf4y1 = self.y1 + h * (25 * a1 / 216 + 1408 * c1 / 2563 + 2197 * d1 / 4104 - e1 / 5)
+		rkf5y1 = self.y1 + h * (16 * a1 / 135 + 6656 * c1 / 12825 + 28561 * d1 / 56430 - 9 * e1 / 50 - 2 * f1 / 55)
+		rkf4y2 = self.y2 + h * (25 * a2 / 216 + 1408 * c2 / 2563 + 2197 * d2 / 4104 - e2 / 5)
+		rkf5y2 = self.y2 + h * (16 * a2 / 135 + 6656 * c2 / 12825 + 28561 * d2 / 56430 - 9 * e2 / 50 - 2 * f2 / 55)
+		return rkf4y1, rkf5y1, rkf4y2, rkf5y2
+
+	def rkf_step(self):
+		R1, R2 = 1, 1
+		h = self.h
+		iters = 0
+		while R1 >= self.error or R2 >= self.error:
+			y1, rkf5y1, y2, rkf5y2 = self.rkf_test(h)
+			R1 = abs(rkf5y1 - y1)
+			R2 = abs(rkf5y2 - y2)
+			R = max(R1, R2)
+			h = self.alpha * h * math.pow(self.error / R, 1.0 / 4.0)
+			iters += 1
+			if iters > self.max_iter:
+				break
+		self.x = self.x + h
+		self.y1 = y1
+		self.y2 = y2
+		return self.x, self.y1
+
+	def __call__(self):
+		x_list = []
+		y_list = []
+		for i in range(self.max_iter):
+			x, y = self.rkf_step()
+			x_list.append(x)
+			y_list.append(y)
+		return x_list, y_list
+
+if __name__ == "__main__":
+	x_list, y_list = RKF(0.8, 0.0001, 0.1, 100, 0, 0, 0)()
+	print(len(x_list))
+	plt.plot(x_list, y_list)
+	plt.title("Runge-Kutta-Fehlberg Method")
 	plt.xlabel("x")
 	plt.ylabel("y")
-	ani.save("output.html", writer="imagemagick")
+	plt.savefig("rkf.png")
+
