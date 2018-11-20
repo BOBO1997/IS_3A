730b123798ec74a7999d8ec37142ed7450293f87
BOBO1997git <yh198595@gmail.com>
2018-11-20 20:34:17 +0900

    modified rkf

--- continuous_exercise/04/rkf.py
+++ continuous_exercise/04/rkf.py
@@ -2,6 +2,22 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+class ORG:
+	def __init__(self, num, r):
+		self.num = num
+		self.range = r
+
+	def f(self, x):
+		return - math.exp(- x) / 2 + 2 * math.exp(- 2 * x) / 5 + 3 * math.sin(x) / 10 + math.cos(x) / 10
+
+	def __call__(self):
+		x_list = np.linspace(0, self.range, self.num)
+		y_list = []
+		for x in x_list:
+			y = self.f(x)
+			y_list.append(y)
+		return x_list, y_list
+
 class RKF:
 	def __init__(self, alpha, error, h, max_iter, x, y1, y2):
 		self.h = h
@@ -46,45 +62,55 @@ class RKF:
 		f2 = self.fy2(self.x + h / 2,
 					  self.y2 - 8 * a1 / 27 + 2 * b1 - 3544 * c1 / 2565 + 1859 * d1 / 4104 - 11 * e1 / 40,
 					  self.y2 - 8 * a2 / 27 + 2 * b2 - 3544 * c2 / 2565 + 1859 * d2 / 4104 - 11 * e2 / 40)
-		rkf4y1 = self.y1 + h * (25 * a1 / 216 + 1408 * c1 / 2563 + 2197 * d1 / 4104 - e1 / 5)
+		rkf4y1 = self.y1 + h * (25 * a1 / 216 + 1408 * c1 / 2565 + 2197 * d1 / 4104 - e1 / 5)
 		rkf5y1 = self.y1 + h * (16 * a1 / 135 + 6656 * c1 / 12825 + 28561 * d1 / 56430 - 9 * e1 / 50 - 2 * f1 / 55)
-		rkf4y2 = self.y2 + h * (25 * a2 / 216 + 1408 * c2 / 2563 + 2197 * d2 / 4104 - e2 / 5)
+		rkf4y2 = self.y2 + h * (25 * a2 / 216 + 1408 * c2 / 2565 + 2197 * d2 / 4104 - e2 / 5)
 		rkf5y2 = self.y2 + h * (16 * a2 / 135 + 6656 * c2 / 12825 + 28561 * d2 / 56430 - 9 * e2 / 50 - 2 * f2 / 55)
 		return rkf4y1, rkf5y1, rkf4y2, rkf5y2
 
 	def rkf_step(self):
-		R1, R2 = 1, 1
+		R = 1
 		h = self.h
+		pre_h = h
 		iters = 0
-		while R1 >= self.error or R2 >= self.error:
+		while R >= self.error:
 			y1, rkf5y1, y2, rkf5y2 = self.rkf_test(h)
 			R1 = abs(rkf5y1 - y1)
 			R2 = abs(rkf5y2 - y2)
 			R = max(R1, R2)
+			pre_h = h
 			h = self.alpha * h * math.pow(self.error / R, 1.0 / 4.0)
 			iters += 1
 			if iters > self.max_iter:
 				break
-		self.x = self.x + h
+		self.x = self.x + pre_h
 		self.y1 = y1
 		self.y2 = y2
-		return self.x, self.y1
+		return pre_h, self.x, self.y1
 
 	def __call__(self):
+		h_list = []
 		x_list = []
 		y_list = []
 		for i in range(self.max_iter):
-			x, y = self.rkf_step()
+			h, x, y = self.rkf_step()
+			h_list.append(h)
 			x_list.append(x)
 			y_list.append(y)
-		return x_list, y_list
+		return h_list, x_list, y_list
 
 if __name__ == "__main__":
-	x_list, y_list = RKF(0.8, 0.0001, 0.1, 100, 0, 0, 0)()
-	print(len(x_list))
+	h_list, x_list, y_list = RKF(0.8, 0.0001, 0.1, 10000, 0, 0, 0)()
+	x_org, y_org = ORG(10000, 40)()
 	plt.plot(x_list, y_list)
+	plt.plot(x_org, y_org)
 	plt.title("Runge-Kutta-Fehlberg Method")
 	plt.xlabel("x")
 	plt.ylabel("y")
 	plt.savefig("rkf.png")
-
+	plt.clf()
+	plt.title("Runge-Kutta-Fehlberg Method")
+	plt.xlabel("x")
+	plt.ylabel("y")
+	plt.plot(h_list)
+	plt.savefig("h.png")
